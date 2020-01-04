23a8a956bed8daa52d10a1ca5c09b8a5d4a23d6a
pham123 <phamdong88@gmail.com>
2020-01-04 16:59:43 +0700

    update excel download for spare part

--- spare-part/index.php
+++ spare-part/index.php
@@ -148,19 +148,7 @@ $oDB = new db();
     </div>
   </div>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="../vendor/jquery/jquery.min.js"></script>
-  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- Core plugin JavaScript-->
-  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
-  <!-- Custom scripts for all pages-->
-  <script src="../js/sb-admin-2.min.js"></script>
-  <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
-  <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script> 
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="../vendor/select/dist/js/bootstrap-select.min.js"></script>
-  <!-- (Optional) Latest compiled and minified JavaScript translation files -->
-  <script src="../vendor/select/dist/js/i18n/defaults-*.min.js"></script>
+  <?php require('../views/template-footer.php'); ?>
 
   <script>
     $.fn.dataTable.ext.search.push(

a20275e6ea4bdb3a940940029932532da417a03e
pham123 <phamdong88@gmail.com>
2020-04-11 08:25:25 +0700

    update

--- home/index.php
+++ home/index.php
@@ -141,7 +141,7 @@ $linkar =  array(
   array('../employees/',$oDB->lang('Employees'),$oDB->lang('EmployeesInformation'),'fa-calendar'),
   array('../news/',$oDB->lang('News'),$oDB->lang('News'),'fa-newspaper'),
   array('../security/',$oDB->lang('Security'),$oDB->lang('Security'),'fa-shield-alt'),
-  array('../thietbido/',$oDB->lang('ME',"Q-Bank"),$oDB->lang('Measure Equipment'),'fa-microscope'),
+  array('../thietbido/',$oDB->lang('ME'),$oDB->lang('Measure Equipment'),'fa-microscope'),
   //
   //
   // 
